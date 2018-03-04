abe948bd9a08eb655dad5603f30a82d96ea1b1da
Simonexc <30417257+Simonexc@users.noreply.github.com>
2018-03-04 20:10:19 +0100

    Added some stuff
    Added background and balloon image.

---
+++ Flying Balloon/Assets/Scripts/Main Game/RepeatBackground.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RepeatBackground : MonoBehaviour {
+
+	private BoxCollider2D groundCollider;
+	private float groundLength;
+
+	// Use this for initialization
+	void Start () {
+		groundCollider = GetComponent<BoxCollider2D> ();
+		groundLength = groundCollider.size.x * transform.localScale.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position.x < -groundLength) {
+			RepositionBackground ();
+		}
+	}
+
+	private void RepositionBackground () {
+		Vector2 newPosition = new Vector2 (transform.position.x + groundLength * 2f, transform.position.y);
+		transform.position = newPosition;
+	}
+}
---
+++ Flying Balloon/Assets/Scripts/Main Game/Scroll.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scroll : MonoBehaviour {
+
+	public float scrollCoefficient = 1; // used for parallax effect
+	private Rigidbody2D rbGround;
+
+	// Use this for initialization
+	void Start () {
+		rbGround = GetComponent<Rigidbody2D> ();
+		rbGround.velocity = new Vector2 (GameControl.scrollSpeed * scrollCoefficient, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
