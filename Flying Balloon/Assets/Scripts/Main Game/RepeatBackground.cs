b6e74c0dc5a9e0a31a3aaff3e6b0ec37e7b3e7e2
Simonexc <30417257+Simonexc@users.noreply.github.com>
2018-08-31 00:13:32 +0200

    Graphics Changes

--- Flying Balloon/Assets/Scripts/Main Game/RepeatBackground.cs
+++ Flying Balloon/Assets/Scripts/Main Game/RepeatBackground.cs
@@ -7,16 +7,24 @@ public class RepeatBackground : MonoBehaviour {
 	public string objectTag = "Ground";
 	public float freeSpace = 0f;
 	public float repositionOffset = 0f;
+	public float fixMove = 0f; // 0 if not "intelligent" reposition
 		
 	private float groundLength; // lenght of the ground
 
 	void Start () {
-		groundLength = GetComponent<Renderer> ().bounds.size.x; // get lenght from the renderer
+		if (fixMove == 0)
+			groundLength = GetComponent<Renderer> ().bounds.size.x; // get lenght from the renderer
 	}
 
 	void Update () {
-		if (transform.position.x < -groundLength-freeSpace-repositionOffset) { // if the ground isn't on the screen
-			RepositionBackground ();
+		if (fixMove == 0) {
+			if (transform.position.x < -groundLength - freeSpace - repositionOffset) { // if the ground isn't on the screen
+				RepositionBackground ();
+			}
+		} else {
+			if (transform.position.x < -fixMove) { // if the ground isn't on the screen
+				RepositionBackground ();
+			}
 		}
 	}
 
@@ -30,7 +38,10 @@ public class RepeatBackground : MonoBehaviour {
 
 
 		// set the position of this background to be on the right of the most right background
-		transform.position = new Vector2 (furthestPosition + freeSpace + groundLength, transform.position.y);
+		if (fixMove == 0)
+			transform.position = new Vector2 (furthestPosition + freeSpace + groundLength, transform.position.y);
+		else
+			transform.position = new Vector2 (furthestPosition + fixMove, transform.position.y);
 	}
 
 }
